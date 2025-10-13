537066f649bd9a9ee850d7c188d22ac8dd40596a
Taha_Afzali <elfo.dp8787@gmail.com>
2025-10-13 13:06:46 +0330

    dbug  [userid]

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -4,11 +4,11 @@ export default function userList() {
   return (
     <div>
       <h1>Users list Page</h1>
-      <Link href="/users/1">user-1</Link>
+      <Link href="/users/1">user 1</Link>
       <br />
-      <Link href="/users/2">user-2</Link>
+      <Link href="/users/2">user 2</Link>
       <br />
-      <Link href="/users/3">user-3</Link>
+      <Link href="/users/3">user 3</Link>
     </div>
   );
 }
