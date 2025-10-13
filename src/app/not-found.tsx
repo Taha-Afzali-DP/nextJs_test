3495dd72faf4ad74ac0b6ca00a2f14d041286c63
Taha_Afzali <elfo.dp8787@gmail.com>
2025-10-13 13:25:29 +0330

    add not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,10 @@
 import "./style-Eror.css";
 
-export default function notFound() {
+const notFound = () => {
   return (
     <div>
       <h1>اشتباه زدی خل</h1>
     </div>
   );
-}
+};
+export default notFound;
