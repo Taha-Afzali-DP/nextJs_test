ab09dd074dd2fe7b76a5c7eab9c38ecfbd990add
Taha_Afzali <elfo.dp8787@gmail.com>
2025-10-13 12:47:11 +0330

    add ProType var

--- src/app/users/[useird]/page.tsx
+++ src/app/users/[useird]/page.tsx
@@ -1,4 +1,8 @@
-export default function userdetils() {
+interface PropsType {
+  params: { userId: string };
+}
+export default function userdetils(props: PropsType) {
+  const userid = props.params.userId;
   return (
     <div>
       <h1>Users detils</h1>
