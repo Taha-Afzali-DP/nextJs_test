1642dcb25e1d5877bdaaa1591e3dcd2f735f9f32
Taha_Afzali <elfo.dp8787@gmail.com>
2025-10-13 12:32:43 +0330

    add links for users

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
+
 export default function userList() {
   return (
     <div>
-      <h1>Users list</h1>
+      <h1>Users list Page</h1>
+      <Link href="/users/1">user-1</Link>
+      <br />
+      <Link href="/users/2">user-2</Link>
+      <br />
+      <Link href="/users/3">user-3</Link>
     </div>
   );
 }
